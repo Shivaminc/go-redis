19002ae38a35c6be0026e6912f8789c347c0324a
Shivaminc. <54238581+Shivaminc@users.noreply.github.com>
2024-09-01 19:57:56 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var ctx = context.Background()
 func main() {
     // Create a new Redis client
     rdb := redis.NewClient(&redis.Options{
-        Addr: "172.17.0.1:6379", // Address of the Redis server
+        Addr: "redis:6380", // Address of the Redis server
     })
 
     // Ping the Redis server to check the connection
